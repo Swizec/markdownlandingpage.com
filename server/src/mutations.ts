1169ff17635a9f1905615f4ef53dbca7e8216232
Swizec Teller <swizec@swizec.com>
2019-12-15 15:08:14 -0800

    Mutation to update user in database

--- server/src/mutations.ts
+++ server/src/mutations.ts
@@ -1,24 +1,57 @@
-import { getItem } from "./dynamodb";
+import { getItem, updateItem } from "./dynamodb";
 
-export const updateItem = async ({
-    userId
-}: {
+type UserParams = {
     userId: string;
-}): Promise<User> => {
+};
+
+export const updateUser = async (_: any, params: UserParams): Promise<User> => {
     // see if user exists
     // if not: update createdAt
     // always update lastSignedInAt
+    const { userId } = params;
 
-    let user = await getItem({
+    const result = await getItem({
         TableName: process.env.USER_TABLE!,
         Key: {
             userId
         }
     });
 
+    let user = result.Item;
+
+    if (user) {
+        // update the user
+        const result = await updateItem({
+            TableName: process.env.USER_TABLE!,
+            Key: { userId },
+            UpdateExpression: "SET lastSignedInAt = :lastSignedInAt",
+            ExpressionAttributeValues: {
+                ":lastSignedInAt": new Date().toISOString()
+            },
+            ReturnValues: "ALL_NEW"
+        });
+
+        user = result.Attributes;
+    } else {
+        // create new user
+        const result = await updateItem({
+            TableName: process.env.USER_TABLE!,
+            Key: { userId },
+            UpdateExpression:
+                "SET createdAt = :createdAt, lastSignedInAt = :lastSignedInAt",
+            ExpressionAttributeValues: {
+                ":createdAt": new Date().toISOString(),
+                ":lastSignedInAt": new Date().toISOString()
+            },
+            ReturnValues: "ALL_NEW"
+        });
+
+        user = result.Attributes;
+    }
+
     return {
         userId,
-        createdAt: user.Item ? user.Item.createdAt : null,
-        lastSignedInAt: user.Item ? user.Item.lastSignedInAt : null
+        createdAt: user ? user.createdAt : null,
+        lastSignedInAt: user ? user.lastSignedInAt : null
     };
 };
--- webapp/src/pages/index.js
+++ webapp/src/pages/index.js
@@ -5,43 +5,47 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-import { Heading, Button } from 'rebass'
-import { useAuth } from 'react-use-auth'
-import { useQuery } from 'react-apollo-hooks'
-import gql from 'graphql-tag'
+import { Heading, Button } from "rebass"
+import { useAuth } from "react-use-auth"
+import { useQuery } from "react-apollo-hooks"
+import gql from "graphql-tag"
 
 const IndexPage = () => {
-    const { isAuthenticated, user, login } = useAuth()
-    const data = useStaticQuery(graphql`
-        query {
-            mdlapi {
-                hello {
-                    world
-                }
-            }
+  const { isAuthenticated, user, login } = useAuth()
+  const data = useStaticQuery(graphql`
+    query {
+      mdlapi {
+        hello {
+          world
         }
-    `)
+      }
+    }
+  `)
 
-    const apolloData = useQuery(gql`
-        query hello {
-            hello {
-                world
-            }
-        }
-    `)
-    const liveData = apolloData.data, loading=apolloData.loading;
-
-    console.log(apolloData)
+  const apolloData = useQuery(gql`
+    query hello {
+      hello {
+        world
+      }
+    }
+  `)
+  const liveData = apolloData.data,
+    loading = apolloData.loading
 
   return (
     <Layout>
-        <SEO title="Markdown Landing Page" />
-        <Heading fontSize={[5, 6, 7]}>Markdown Landing Page</Heading>
-        <p>write a landing page for anything</p>
-        <p>From GraphQL server: {liveData ? liveData.hello.world : data.mdlapi.hello.world}</p>
-        {loading ? <p>Fetching ..</p> : null}
-        {isAuthenticated() ? <p>hello {user.nickname}</p> : null}
-        <Button bg='highlight' onClick={login}>Get started</Button>
+      <SEO title="Markdown Landing Page" />
+      <Heading fontSize={[5, 6, 7]}>Markdown Landing Page</Heading>
+      <p>write a landing page for anything</p>
+      <p>
+        From GraphQL server:{" "}
+        {liveData ? liveData.hello.world : data.mdlapi.hello.world}
+      </p>
+      {loading ? <p>Fetching ..</p> : null}
+      {isAuthenticated() ? <p>hello {user.nickname}</p> : null}
+      <Button bg="highlight" onClick={login}>
+        Get started
+      </Button>
     </Layout>
   )
 }

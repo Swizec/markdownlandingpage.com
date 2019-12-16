0a0bd9245015c061c113335103e2f5527a672d47
Swizec Teller <swizec@swizec.com>
2019-12-15 17:37:07 -0800

    Noneditable page for normal people

--- webapp/src/pages/landingPage.js
+++ webapp/src/pages/landingPage.js
@@ -25,12 +25,15 @@ function useContentFromServer({ userId, pageId, setPageContent }) {
     { variables: { userId, pageId } }
   )
   useEffect(() => {
-    setPageContent(data.page.content)
+    if (data) {
+      setPageContent(data.page.content)
+    }
   }, [data])
 }
 
 const LandingPage = ({ pageContext }) => {
   const { pageName, pageId, userId } = pageContext
+  const { isAuthenticated, user } = useAuth()
 
   // initiate state with compile-time data
   const [pageContent, setPageContent] = useState(pageContext.content)
@@ -38,21 +41,30 @@ const LandingPage = ({ pageContext }) => {
   useContentFromServer({ userId, pageId, setPageContent })
 
   const renderedPage = useRemark(pageContent)
+  const shouldBeEditable = isAuthenticated() && user.sub === userId
 
   return (
     <Layout>
       <SEO title={pageName} />
-      <Heading fontSize={[5, 6, 7]}>{pageName}</Heading>
 
-      <Flex>
-        <Box width={1 / 2}>
-          <Textarea
-            value={pageContent}
-            onChange={ev => setPageContent(ev.target.value)}
-          />
-        </Box>
-        <Box width={1 / 2}>{renderedPage}</Box>
-      </Flex>
+      {shouldBeEditable ? (
+        <>
+          <Heading fontSize={[5, 6, 7]}>{pageName}</Heading>
+          <Flex>
+            <Box width={1 / 2}>
+              <Textarea
+                value={pageContent}
+                onChange={ev => setPageContent(ev.target.value)}
+              />
+            </Box>
+            <Box width={1 / 2}>{renderedPage}</Box>
+          </Flex>
+        </>
+      ) : (
+        <Flex>
+          <Box width={1}>{renderedPage}</Box>
+        </Flex>
+      )}
     </Layout>
   )
 }
